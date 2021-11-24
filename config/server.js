0f17026be6cd8519c0756665f2d33a51803de839
AdheCode <adhep86@gmail.com>
2021-11-24 16:08:00 +0700

    initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0643982c23f81d2160ef3db8e30299cd'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '33d47153-6114-44be-bece-e3b7b3971ed3'
+};
